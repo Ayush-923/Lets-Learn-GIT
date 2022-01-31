254457ad8a3f10a72922666c7521ff69d52612d0
Ayush Kumar Gupta <ayush.ag751@gmail.com>
2022-01-31 16:09:44 +0530

    change For diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 int main()
 {
-    int a = 5;
+    int a = 6;
     int b = 10;
     int c = a-b;
-    cout<<"Differance of 5 & 10 is: "<< c <<endl;
+    cout<<"Differance of 6 & 10 is: "<< c <<endl;
     return 0;
 }
